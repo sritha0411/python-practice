b1f7df8989caa6fcd13883928411474b3e1f594f
sritha0411 <stpsritha@gmail.com>
2026-04-08 12:11:24 +0530

    calculateProduct-handout-student

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
-def calculateSum(a,b):
-    sum=a+b
-    return sum
-a=int(input())
-b=int(input())
-print(calculateSum(a,b))
-
-
+def calculateProduct(x,y,z):
+    product=x*y*z
+    return product
+x=float(input())
+y=float(input())
+z=float(input())
+result=calculateProduct(x,y,z)
+print(result)
