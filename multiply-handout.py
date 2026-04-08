0574abe56e59c14a35001d342593ee1bb653be42
sritha0411 <stpsritha@gmail.com>
2026-04-08 12:28:16 +0530

    multiply-handout-student

---
+++ multiply-handout.py
@@ -0,0 +1,6 @@
+def multiply(a,b):
+    result=a*b
+    return result
+a=int(input())
+b=int(input())
+print(multiply(a,b))
--- solution.py
+++
@@ -1,7 +0,0 @@
-def areaofcircle(r):
-    pi=3.14159
-    area=float(pi*r**2)
-    return area
-r=float(input())
-b=areaofcircle(r)
-print(round(b,2))
