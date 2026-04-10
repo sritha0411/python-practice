2b3822a5cd2d758cacc4ebeff6e79cac6a866da5
sritha0411 <stpsritha@gmail.com>
2026-04-10 10:19:47 +0530

    collatz_conjecture

---
+++ collatz_conjecture_solution.py
@@ -0,0 +1,11 @@
+def required_steps(n):
+    steps = 0
+    while(n>1):
+        if n%2==0:
+            n=n//2
+        else:
+            n=(3*n)+1
+        steps += 1
+    return steps
+
+print(required_steps(int(input())))
