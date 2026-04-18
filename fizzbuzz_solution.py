52ad8d0408eacd5d8869db065096d3664757245e
sritha0411 <stpsritha@gmail.com>
2026-04-18 16:58:41 +0530

    fizzbuzz

---
+++ fizzbuzz_solution.py
@@ -0,0 +1,11 @@
+def fizzbuzz(n):
+    if n%3==0 and n%5==0:
+        print("FizzBuzz")
+    elif n%3==0:
+        print("Fizz")
+    elif n%5==0:
+        print("Buzz")
+    else:
+        print(n)
+
+fizzbuzz(int(input()))
